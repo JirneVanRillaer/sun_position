2748be685241e5225466c08537fb3b34d6135857
JirneVanRillaer <81569558+JirneVanRillaer@users.noreply.github.com>
2021-11-19 12:12:24 +0100

    Update sun_position.h

--- sun_position.h
+++ sun_position.h
@@ -1,21 +1,26 @@
-/* created by Jirne Van Rillaer */
-
 #ifndef SUN_POSITION_H
 #define SUN_POSITION_H
 
 #include <Arduino.h>
 #include <RTClib.h>
 
-float days_passed(DateTime t);
-float localTime(DateTime t);
-int LSTM (int offset);
-float EOT (DateTime t);
-float TC(int offset, DateTime t, float longitude);
-float LST(int offset, DateTime t, float longitude);
-float HRA(int offset, DateTime t, float longitude);
-float DA(DateTime t);
+struct DT {
+    int yy;
+    int mm;
+    int dd;
+    int h;
+    int m;
+};
 
-float EA(DateTime t, float offset, float latitude, float longitude);
-float AA(DateTime t, float offset, float latitude, float longitude);
+float days_passed(DT t);
+float localTime(DT t);
+int LSTM (int offset);
+float EOT (DT t);
+float TC(int offset, DT t, float longitude);
+float LST(int offset, DT t, float longitude);
+float HRA(int offset, DT t, float longitude);
+float DA(DT t);
+float EA(DT t, float offset, float latitude, float longitude);
+float AA(DT t, float offset, float latitude, float longitude);
 
 #endif
